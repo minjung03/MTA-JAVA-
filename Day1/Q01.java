dd091692c1a11a4e7d68a75859f21c0363a52a94
minjung Kim <ikwmjj@naver.com>
2022-01-04 09:20:25 +0900

    Add files via upload

---
+++ Day1/Q01.java
@@ -0,0 +1,17 @@
+package Day1;
+
+// MTA 기출 1
+public class Q01 {
+	public static void main(String[] args) {
+		
+		  double pi = Math.PI; // 3.141593
+		  
+		  System.out.format("PI is %.3f%n", pi); 
+		  // 소수점 3자리까지 출력 (4번째 자리에서 반올림***) => 3.14"2"
+		  System.out.format("PI is %.0f%n", pi);
+		  // 소수로 만들지 말 것 => 3
+		  System.out.format("PI is %09f%n", pi); 
+		  // 소수점도 하나의 자리이다**
+		  // "앞"의 빈자리는 0으로 채우고, 전체 9자리까지 출력 => 03.141593
+		}
+}
---
+++ Day1/Q09.java
@@ -0,0 +1,27 @@
+package Day1;
+// MTA 기출 6
+public class Q09 {
+
+	public static void main(String[] args) {
+		printInt();
+	}
+	
+	public static void printInt() {
+		if(true) {
+			int num = 1; // if 블럭 안에서 선언되었으니 이 안에서만 존재함
+			if(num>0) {
+				num++;
+			}
+		}
+		
+		int num = 1; 
+		addOne(num); // 영향 없음
+		num = num - 1;
+		System.out.println(num); // 0
+	}
+	
+	public static void addOne(int num) {
+		num = num+1; // 위의 num과는 다른 변수이다
+	}
+
+}
